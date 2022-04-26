12e81655b032153da5179145284ffdca1ccbe24d
Mraimundo <fmdumundo18@gmail.com>
2022-04-25 23:54:38 -0300

    add component Header and Player

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import { Header } from "../components/Header";
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <h1>Index</h1>;
 }
